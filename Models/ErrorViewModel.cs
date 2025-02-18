2e51c89172fb23374048e3d027800a5381315ccd
Femi <femi_ogun@outlook.com>
2025-02-18 00:59:02 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mopra_Landing_Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
